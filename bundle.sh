b911720543561a9d5d87d0511e0ea494887a64b9
Ryan Miller <RyanMillerC@outlook.com>
2024-04-19 08:05:25 -0700

    Add files from Windows 11 box

---
+++ bundle.sh
@@ -0,0 +1,8 @@
+curl -L -s https://github.com/mrjosh/helm-ls/releases/download/master/helm_ls_windows_amd64.exe --output helm_ls.exe
+
+curl -L -s https://github.com/artempyanykh/marksman/releases/latest/download/marksman.exe --output marksman.exe
+
+# Download this
+https://github.com/valentjn/ltex-ls/releases/download/16.0.0/ltex-ls-16.0.0-windows-x64.zip
+
+https://releases.hashicorp.com/terraform-ls/0.33.0/terraform-ls_0.33.0_windows_amd64.zip
